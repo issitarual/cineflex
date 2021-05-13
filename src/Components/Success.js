cd20db43d3a6f24fdcacb925fbe439c1b34d54b2
Laura Tissi <laura.tracierra@gmail.com>
2021-05-13 11:33:25 -0300

    tirei coisa a mais do código

--- src/Components/Success.js
+++ src/Components/Success.js
@@ -1,5 +1,4 @@
 import { Link } from 'react-router-dom';
-import { useState, useEffect } from 'react';
 
 export default function Success ({ information, setId, setInformation}) {
     const {movie, buyer, seats} = information;
